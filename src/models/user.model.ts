49b6102e0940f0a53e1700bc85022f1990846bfd
sshreya007 <shreyakhadka007@gmail.com>
2026-01-15 19:31:43 +0545

    Working on user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);
