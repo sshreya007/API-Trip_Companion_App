6c364fbcadacf1e1af8b5a754bfd4f5250560a40
sshreya007 <shreyakhadka007@gmail.com>
2026-01-15 19:32:43 +0545

    Working on auth dto

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,11 @@
+export interface RegisterDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginDto {
+  email: string;
+  password: string;
+}
